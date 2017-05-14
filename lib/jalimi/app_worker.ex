cc111f3ea3e5063cb2807d253642ad9f744b228b
Jordan Alimi-Tollgreve <jordanalimitollgreve@gmail.com>
2017-05-14 19:41:18 -0400

    Try some Elixir magic.

--- lib/jalimi/app_worker.ex
+++ lib/jalimi/app_worker.ex
@@ -2,7 +2,7 @@ defmodule Jalimi.AppWorker do
 
   def start_link do
     port = System.get_env("PORT") |> String.to_integer
-    Plug.Adapters.Cowboy.http MyRouter, [], port: port
+    { :ok, _ } = Plug.Adapters.Cowboy.http MyRouter, [], port: port
   end
 
 end
--- lib/jalimi/my_plug.ex
+++ lib/jalimi/my_plug.ex
@@ -1,5 +1,5 @@
 defmodule MyRouter do
-  import Plug.Router
+  use Plug.Router
 
   if Mix.env == :dev do
     import Plug.Debugger
@@ -9,10 +9,12 @@ defmodule MyRouter do
   plug :dispatch
 
   get "/" do
+    put_resp_content_type(conn, "text/plain")
     send_resp(conn, 200, "HELLO")
   end
 
   match _ do
+    put_resp_content_type(conn, "text/plain")
     send_resp(conn, 404, "oops")
   end
 end
