61c50b0ebcf529a0a6c306946e4d9ed1bc6c05d6
Jordan Alimi-Tollgreve <jordanalimitollgreve@gmail.com>
2017-05-14 19:24:35 -0400

    Remove match and dispatch from router.

--- lib/jalimi/my_plug.ex
+++ lib/jalimi/my_plug.ex
@@ -1,12 +1,9 @@
 defmodule Router do
-  use Plug.Router
+  import Plug.Router
 
   if Mix.env == :dev do
     use Plug.Debugger
   end
-
-  Plug :match
-  Plug :dispatch
 
   get "/" do
     send_resp(conn, 200, "HELLO")
