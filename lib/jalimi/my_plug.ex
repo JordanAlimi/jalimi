593ae1742bd9c4bd38a7273372d41249b7292148
Jordan Alimi-Tollgreve <jordanalimitollgreve@gmail.com>
2017-05-14 19:33:57 -0400

    Modify name of router and create default route.

--- lib/jalimi/my_plug.ex
+++ lib/jalimi/my_plug.ex
@@ -1,11 +1,17 @@
-defmodule Router do
+defmodule MyRouter do
   import Plug.Router
 
   if Mix.env == :dev do
     import Plug.Debugger
   end
 
+  plug :match
+  plug :dispatch
+
   get "/" do
-    conn |> Plug.Conn.send_resp(conn, 200, "HELLO")
+    send_resp(conn, 200, "HELLO")
   end
+
+  match _ do
+    send_resp(conn, 404, "oops")
 end
