fa903b0da8213484174d4effbc40a90ee0f6e8b1
Jordan Alimi-Tollgreve <jordanalimitollgreve@gmail.com>
2017-05-14 20:27:30 -0400

    Add missing closing parantheses.

--- lib/jalimi/my_plug.ex
+++ lib/jalimi/my_plug.ex
@@ -11,12 +11,12 @@ defmodule MyRouter do
   get "/" do
     conn
     |> put_resp_content_type("application/json")
-    |> send_resp(200, Poison.encode(%{status_code: "200", message: "You have succeeded."})
+    |> send_resp(200, Poison.encode(%{status_code: "200", message: "You have succeeded."}))
   end
 
   match _ do
     conn
     |> put_resp_content_type("application/json")
-    |> send_resp(404, Poison.encode(%{status_code: "404", message: "Page not found."})
+    |> send_resp(404, Poison.encode(%{status_code: "404", message: "Page not found."}))
   end
 end
